1e775a15706d330b4212cb810280ff8c699c8336
CJ-3-Bz <okc.0137rpm@yahoo.ne.jp>
2020-02-05 20:16:11 +0900

    hello

---
+++ django_app/hello/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Friend
+
+class FriendForm(forms.ModelForm):
+     class Meta:
+        model = Friend
+        fields = ['name','mail','gender','age','birthday']
+
+
+class FindForm(forms.Form):
+    find = forms.CharField(label = 'Find',required=False)
+
+class CheckForm(forms.Form):
+    str = forms.CharField(label='Name')
+    max = forms.CharField(label='Max',max_length=10)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        str = cleaned_data['str']
+        if (str.lower().startswith('no')):
+            raise forms.ValidationError('You input NO!')
